ee8a66c6ad6e95909c3ac0572a2a4dc973c1bbd4
CanvasL <746591811@qq.com>
2024-03-29 16:37:46 +0800

    chore: backend api

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://127.0.0.1:8576/api/v1/",
+  baseURL: "https://api.pyra.wtf/api/v1/",
   headers: {
     accept: "application/json"
   }
